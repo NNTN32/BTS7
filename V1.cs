e53451bd6626b761220c55183315e2257cc20511
binhq <thanhnhanna102003@gmail.com>
2023-09-22 14:43:08 +0700

    Them class V1

---
+++ V1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SangT7
+{
+    internal class V1
+    {
+        private string ID;
+
+        public string ID1 { get => ID; set => ID = value; }
+    }
+}
